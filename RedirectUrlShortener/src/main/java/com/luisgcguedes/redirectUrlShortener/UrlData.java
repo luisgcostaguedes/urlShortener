1bf2ebd2cca9f1230feb0308b4a211df38e3184b
Luis Guilherme C G <51034261+luisgcostaguedes@users.noreply.github.com>
2024-11-20 16:38:26 -0300

    add all files

---
+++ RedirectUrlShortener/src/main/java/com/luisgcguedes/redirectUrlShortener/UrlData.java
@@ -0,0 +1,16 @@
+package com.luisgcguedes.redirectUrlShortener;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class UrlData {
+    private String originalUrl;
+    private long expirationTime;
+}
